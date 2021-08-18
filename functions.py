a4a1ab363315489dd8b0fbbfb6d91ed2bd0b928a
Daniel Diaz <daniel.diaz.bedoya@outlook.com>
2021-08-18 16:05:21 -0500

    Función para obtener listado de directorios agregada

---
+++ functions.py
@@ -0,0 +1,5 @@
+import os
+
+def get_dirs(path='.'):
+    ls_dir = os.listdir(path)
+    return ls_dir
---
+++ testing.py
@@ -0,0 +1,20 @@
+import unittest
+import functions 
+
+class TestGetDirs(unittest.TestCase):
+    def test_empty_dir(self):
+        module_res = functions.get_dirs(path='./empty_dir')
+        sol = []
+        self.assertEqual(module_res, sol, 
+                f'Your solution is {module_res} rather than {sol}')
+    def test_folders_dir(self):
+        module_res = functions.get_dirs(path='./Referencias')
+        sol = ['AI','cripto','desarrollo'] 
+        self.assertEqual(module_res, sol, 
+                f'Your solution is {module_res} rather than {sol}')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()    
